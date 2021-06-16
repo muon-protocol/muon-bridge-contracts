fb05be9e1fd52e6ccfce45a91578562b5caa87c9
Sadegh Teymouri <sadeghte@gmail.com>
2021-06-16 13:22:39 +0430

    deploy with old muon contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,29 @@
 var bridge = artifacts.require('./MuonBridge.sol')
 var muon = artifacts.require('./MuonV01.sol')
 
+function parseArgv(){
+	let args = process.argv.slice(2);
+	let params = args.filter(arg => arg.startsWith('--'))
+	let result = {}
+	params.map(p => {
+		let [key, value] = p.split('=');
+		result[key.slice(2)] = value === undefined ? true : value
+	})
+	return result;
+}
+
 module.exports = function (deployer) {
 	deployer.then(async () => {
-		let deployedMuon = await deployer.deploy(muon);
-		let deployedBridge = await deployer.deploy(bridge, deployedMuon.address)
+		let params = parseArgv()
+
+		let muonAddress = null
+		if(!!params['muonAddress']){
+			muonAddress = params['muonAddress'];
+		}
+		else{
+			let deployedMuon = await await deployer.deploy(muon);
+			muonAddress = deployedMuon.address;
+		}
+		let deployedBridge = await deployer.deploy(bridge, muonAddress)
 	})
 }
